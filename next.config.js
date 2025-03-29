c862a68574df7ce080fc599bdd7bf0be72fe1076
Jeremy Laue <jeremystuartlaue@gmail.com>
2025-03-28 21:38:14 -0700

    bug fixes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig = {
-    output: "export",
-    basePath: "/jeremys-film-reviews",
-    assetPrefix: "/jeremys-film-reviews",
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  output: "export",
+  basePath: isProd ? "/jeremys-film-reviews" : "",
+  assetPrefix: isProd ? "/jeremys-film-reviews" : "",
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
